b26a5387f633674bdc09042f0a79a8cb6e40ba13
Yash Karthik <52369876+YashKarthik@users.noreply.github.com>
2023-02-07 08:53:55 +0530

    1 - unstoppable

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -45,6 +45,37 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Execution', async function () {
         /** CODE YOUR SOLUTION HERE */
+        
+        /** Potential Attack One:
+         * The ReceiverUnstoppable has a check to determine reject unexpected flash loans
+         * if (initiator != address(this) || msg.sender != address(pool) || token != address(pool.asset()) || fee != 0)
+         * fee != 0; we could try this.
+         * the fee, is calculated using flashFee():
+         * if (block.timestamp < end && _amount < maxFlashLoan(_token)) {
+         *     return 0;
+         * } else {
+         *     return _amount.mulWadUp(FEE_FACTOR);
+         * }
+         * So if we can get the amount to be equal to maxFlashLoan of the pool, it'll give a non-zero fee.
+         * But this amount is set during function call, which is called by the receiver, we cannot mess with this.
+         * But what if we reduce the pool's maxFlashLoan()
+         * It's calculated by calling totalASsets() which returns asset.balanceOf(address(this));
+         * So if we can transfer out tokens from the pool, we could reduce the value returned by maxFlashLoan().
+         */
+
+        /** Attack that works: influence the checks run before flashLoan.
+         * Of all the checks, we may only influence this one:
+         * if (convertToShares(totalSupply) != balanceBefore) revert InvalidBalance(); // enforce ERC4626 requirement
+            * convertToShares(totalSupply) is same as totalSupply.mulDivDown(totalSupply, asset.balanceOf(address(this)));
+            * It checks (totalSupply * totalSupply) / (asset.balanceOf(address(this))) = asset.balanceOf(address(this));
+            * x * x / y = y => x^2 = y^2 => x = y;
+            * So the only way this flashLoan succeeds is when the num tokens of DVT and oDVT are the same.
+            * We have 10 DVT ourself, we can use this to disturb x = y;
+            * But we must not send the tokens via the deposit function as that would increase the size of the pie => maintaining the ratio.
+            * We send using the ERC20 transfer function.
+         */
+            
+        await token.transfer(vault.address, INITIAL_PLAYER_TOKEN_BALANCE);
     });
 
     after(async function () {
